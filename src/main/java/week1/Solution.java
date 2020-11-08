3f5823604b32c71884264b874b1c4f107e21c64e
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-11-08 11:12:33 +0900

    [week1][programmers] 트리트리오 문제 추가

---
+++ src/main/java/week1/Solution.java
@@ -0,0 +1,12 @@
+/*
+  https://programmers.co.kr/learn/courses/30/lessons/68937
+  [프로그래머스][월간 코드 챌린지 시즌1] 트리 트리오 중간 값
+ */
+package week1;
+
+public class Solution {
+    public int solution(int n, int[][] edges) {
+        int answer = 0;
+        return answer;
+    }
+}
