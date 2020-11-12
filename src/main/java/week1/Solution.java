a3c7053e38ce9f579d18f6252cba7d86384df281
SONG22 <ansongi527@gmail.com>
2020-11-13 00:20:04 +0900

    2차 시도 오답

--- src/main/java/week1/Solution.java
+++ src/main/java/week1/Solution.java
@@ -8,70 +8,62 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    // 1. 중복 제외 임의의 3개를 뽑는다
-    // 2. 거리를 구해서 최댓값을 리턴한다
+
     static LinkedList<Integer> [] list;
-    static int max;
+    static int []dis;
+    static int x,max,cnt, a,b,c;
+
     public int solution(int n, int[][] edges) {
         list = new LinkedList[n+1];
-        for(int i=1; i<=n; i++) list[i] = new LinkedList<>();
-        for(int i=0; i<n-1; i++){
+        dis = new int[n+1];
+
+        for(int i=0;i<=n; i++) list[i] = new LinkedList<>();
+
+        for(int i=0; i<edges.length; i++){
             int s = edges[i][0];
             int e = edges[i][1];
             list[s].add(e);
             list[e].add(s);
         }
-        boolean []check = new boolean[n+1];
-        max = 0;
-//        solve(0,1,n, check);
-        for(int i=1; i<n; i++){
+        x=max=0;
+        dfs(1);  // 가장 깊은 점 찾기
+        a = x;
 
+        x=max=0;
+        dis = new int[n+1];
+        dfs(a);
+        b = x;
+
+        for(int i=1; i<=n; i++) {
+            if(i!=a && dis[i] == max) cnt++;
         }
-        return max;
+
+        if(cnt >=2) return max;
+        else {
+            x=max=0;
+            dis = new int [n+1];
+            dfs(b);
+            c = x;
+            cnt=0;
+            for(int i=1; i<=n; i++){
+                if(i!=b && dis[i] == max)cnt++;
+            }
+            if(cnt >=2) return max;
+            return max-1;
+        }
     }
 
-    static int []res = new int [3];
-    public void solve(int idx, int start, int n, boolean []check){
-        if(idx==3) {
-//            for(int i=0; i<3; i++)
-//            System.out.print(res[i]+" ");
-//            System.out.println();
-            int ab = bfs(res[0],res[1]);
-            int bc = bfs(res[1],res[2]);
-            int ca = bfs(res[2],res[0]);
-            int r= (ab+bc+ca)/3;
-//            System.out.println(ab+" "+bc+" "+ca+" "+r);
-            if(max<r)max=r;
-            return;
+    static void dfs(int start){
+        if(max < dis[start]) {
+            max = dis[start];
+            x = start;
         }
-        for(int i=start; i<=n; i++){
-            if(check[i]) continue;
-            res[idx] = i;
-            check[i] = true;
-            solve(idx+1, start+1, n, check);
-            check[i] = false;
-        }
-    }
-    public int bfs(int start, int end){
-        boolean []c= new boolean[list.length];
-        c[start]=true;
-        Queue<Integer> q= new LinkedList<>();
-        Queue<Integer> d= new LinkedList<>();
-        q.add(start);d.add(0);
-        int dis = 0;
-        while(!q.isEmpty()){
-            int now =q.poll();
-            dis =d.poll();
-            if(now == end) {
-                break;
-            }
-            for(int next : list[now]){
-                if(c[next])continue;
-                q.add(next);
-                d.add(dis+1);
-                c[next]=true;
+
+        for(int next: list[start]){
+            if(dis[next] == 0){
+                dis[next] = dis[start]+1;
+                dfs(next);
             }
         }
-        return dis;
     }
 }
