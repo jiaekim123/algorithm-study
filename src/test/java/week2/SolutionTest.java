9613f16d18bd4389106c5145eff4ee78d182cccc
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-11-14 20:59:41 +0900

    [week2] 동적계획법 N으로 표현 문제 테스트 코드 추가

---
+++ src/test/java/week2/SolutionTest.java
@@ -0,0 +1,14 @@
+package week2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution() {
+        assertEquals(4, new Solution().solution(5, 12));
+        assertEquals(3, new Solution().solution(2, 11));
+    }
+}
