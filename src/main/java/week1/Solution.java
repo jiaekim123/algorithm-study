c3fdd63e788ad5a8898409ce146c90f249c25610
SONG22 <ansongi527@gmail.com>
2020-11-15 00:41:06 +0900

    3차 시도 오답.....테케 18 런타임 에러 났는데 모르겠쒀요

--- src/main/java/week1/Solution.java
+++ src/main/java/week1/Solution.java
@@ -5,65 +5,62 @@
 package week1;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Solution {
 
     static LinkedList<Integer> [] list;
     static int []dis;
-    static int x,max,cnt, a,b,c;
+    static int cnt=0, max=0, leaf=1;
 
     public int solution(int n, int[][] edges) {
         list = new LinkedList[n+1];
-        dis = new int[n+1];
-
         for(int i=0;i<=n; i++) list[i] = new LinkedList<>();
-
+        dis = new int [n+1];
         for(int i=0; i<edges.length; i++){
             int s = edges[i][0];
             int e = edges[i][1];
             list[s].add(e);
             list[e].add(s);
         }
-        x=max=0;
-        dfs(1);  // 가장 깊은 점 찾기
-        a = x;
 
-        x=max=0;
-        dis = new int[n+1];
-        dfs(a);
-        b = x;
-
-        for(int i=1; i<=n; i++) {
-            if(i!=a && dis[i] == max) cnt++;
-        }
-
-        if(cnt >=2) return max;
-        else {
-            x=max=0;
-            dis = new int [n+1];
-            dfs(b);
-            c = x;
-            cnt=0;
-            for(int i=1; i<=n; i++){
-                if(i!=b && dis[i] == max)cnt++;
-            }
-            if(cnt >=2) return max;
-            return max-1;
+        solve(1,n, false);  // 임의의 정점 1에서 가장 깊은 리프노드를 구한다 (정점 A)
+        solve(leaf, n, true); // 구한 리프노드 leaf 에서 (가장 긴) 다른 리프노드를 구한다  (정점 B)
+        if(cnt>=2) return max; // 구한 리프노드 갯수가 2개 이상일 때 중간값의 최댓값은 트리의 지름이다
+        else{ // 구한 리프노드 갯수가 1개 일 때 정점 C를 확인해야 한다
+            solve(leaf, n, true);  // 리프노드를 구한다 (정점 C)
+            if(cnt>=2) return max; // 구한 리프노트 C가 2개 이상이면 중간값의 최댓값은 트리의 지름이다
+            else return max-1; // 정점이 1개만 나오면 중간값의 최댓값은 트리의 지름-1 이 된다.
         }
     }
 
-    static void dfs(int start){
-        if(max < dis[start]) {
-            max = dis[start];
-            x = start;
+    static void solve(int start, int n, boolean T){
+        cnt = 0;
+        max = 0;
+        dis = new int [n+1];
+        dfs(start, 0, T);
+    }
+
+    static void dfs(int cur, int len, boolean T){
+        if(dis[cur]==1) return; // 방문한 노드면 return
+
+        if(T == false){ // 리프노드가 아닐 때
+            if(len> max){
+                leaf = cur; // 리프노드 업데이트
+                max = len;
+            }
+        }
+        else{ // 리프노드 일 때
+            if(len>max){ //max 가 업데이트 되면
+                leaf = cur; // 리프노드 업데이트
+                cnt = 1; // cnt 는 다시 1이 된다
+                max = len;
+            }
+            else if(len == max) cnt ++; //len 과 처음구한 (max)값과 같을 때 갯수 증가
         }
 
-        for(int next: list[start]){
-            if(dis[next] == 0){
-                dis[next] = dis[start]+1;
-                dfs(next);
-            }
+        dis[cur] = 1; // 방문 체크
+        for(int next : list[cur]){
+            dfs(next, len+1, T);
         }
     }
 }
