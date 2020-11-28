1f8b5f2a0f2f3d2ecb777d68a95148127a1d380d
SONG22 <ansongi527@gmail.com>
2020-11-28 21:56:31 +0900

    [week3] 순위 - 2차시도 정답 블로그참고(플로이드워샬)

--- src/main/java/week3/Solution.java
+++ src/main/java/week3/Solution.java
@@ -8,69 +8,39 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 class Solution {
-    static class Node implements Comparable<Node>{
-        int name;
-        LinkedList<Integer> child = new LinkedList<>();
-        int value = 0;
-        Node(int name){
-            this.name = name;
-        }
-        @Override
-        public int compareTo(Node o) {
-            if(value == -1) return -1;
-            if(this.value< o.value) return 1;
-            else if(this.value> o.value) return -1;
-            else return 0;
-        }
-    }
+
     public int solution(int n, int[][] results) {
-
-        LinkedList<Node> nodes = new LinkedList<>();
-        for(int i=0; i<n+1; i++){
-            nodes.add(new Node(i));
-            if(i==0)nodes.get(0).value = -1;
-        }
-        for(int i=0; i<results.length; i++){
-            int win = results[i][0];
-            int lose = results[i][1];
-            // 자식들을 올린다
-            nodes.get(win).child.add(lose);
-            // 계층
-            if(nodes.get(win).value >= nodes.get(lose).value){
-                nodes.get(lose).value = nodes.get(win).value+1;
-            }else if(Math.abs(nodes.get(win).value-nodes.get(win).value)!=1){
-                nodes.get(win).value = nodes.get(lose).value-1;
+        int answer = 0;
+        int maxV = 1000000;
+        int [][]map = new int [n+1][n+1];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                map[i][j] = maxV;
             }
-            LinkedList<Integer> downList = nodes.get(lose).child;
-            for(int c : downList){
-                if(nodes.get(lose).value >= nodes.get(c).value)
-                    nodes.get(c).value= nodes.get(lose).value+1;
-            }
-//            System.out.println("win "+nodes.get(win).name+" "+nodes.get(win).value);
-//            System.out.println("lose "+nodes.get(lose).name+" "+nodes.get(lose).value);
         }
-
-        // value 기준으로 정렬
-        Collections.sort(nodes);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int stopIndex = -1;
-        for(int i=0; i<n; i++){
-//            System.out.println("NAME : "+nodes.get(i).name+ " "+nodes.get(i).value+" ");
-            if(map.containsKey(nodes.get(i).value)){
-                int name = map.get(nodes.get(i).value);
-                if(!nodes.get(name).child.contains( nodes.get(i).name )){
-                    stopIndex = i;
+        for(int []e : results){
+            map[e[0]][e[1]] = 1;
+        }
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++) {
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
                 }
-//                System.out.println(name+ " " + nodes.get(i).name);
-                if(stopIndex!= -1) break;
-            }else
-                map.put(nodes.get(i).value, nodes.get(i).name); // key - value
+            }
         }
-
-//        System.out.println(stopIndex+" ans wer "+ (n-stopIndex));
-
-
-
-        return n-stopIndex;
+        for(int i=1; i<=n; i++){
+            boolean f = true;
+            for(int j=1; j<=n ; j++){
+                if(i==j) continue;
+                if(map[i][j] == maxV && map[j][i] == maxV){
+                    f = false;
+                    break;
+                }
+            }
+            if(f) answer++;
+        }
+        return answer;
     }
 }
