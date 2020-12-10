39f39418115fbff7a87a38ecd23241dcccca0c8c
jiae <hirlawldo413@gmail.com>
2020-12-10 12:16:39 +0900

    [week5] 디스크컨트롤러 문제 해결
    1. jobs를 요청시간 순서대로 정렬한다.
    (거지같이 써도 찰떡같이 알아듣고 고쳐주는 인텔리제이 좋네요)
    2. PriorityQueue를 만들고, queue에 넣은 총 갯수, queue에서 꺼내온 총 갯수, 걸린 총 시간, 현재 시간을 저장하는 값들을 초기화 한다.
    3. jobs에 있는 애들을 모두 queue에 넣고 꺼내올때까지 반복한다.
        - 현재 시간보다 request time이 이전인애가 jobs의 맨 앞에 있다면 queue에 넣는다.
        - queue에 값이 있으면 꺼내서 job을 수행한 시간만큼 현재 시간을 올리고, 전체 시간에 반영한다.
        - 만약 큐에 아무것도 없다면 현재 시간을 jobs의 첫번째 request time으로 맞춘다.
    4. totalTime / jobs.length을 int값으로 반환한다.

--- src/main/java/week5/Solution.java
+++ src/main/java/week5/Solution.java
@@ -11,25 +11,31 @@ import java.util.Queue;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int totalTime = 0;
-        // jobs를 request순으로 정렬
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
 
-        int i = 0;
-        int time = 0;
+        // jobs를 요청시간 순서대로 정렬
+        Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
+
+        int i = 0; // queue 에 넣은 총 갯수
+        int count = 0; // queue 에서 꺼내온 총 갯수
+        int time = 0; // 현재 시간
+        int totalTime = 0; // 총 걸린 시간
         Queue<Job> queue = new PriorityQueue<>();
-        queue.offer(new Job(jobs[i][0], jobs[i][1]));
-        i++;
-        while (!queue.isEmpty()) {
-            // queue에서 Job을 수행하고 time을 올리고 totalTime에 반영한다.
-            Job job = queue.poll();
-            time += job.workingTime;
-            totalTime += (time - job.requestTime);
 
+        while (count < jobs.length){
             // 현재 시간보다 requestTime이 작은 애가 있으면 큐에 넣는다.
             while (i < jobs.length && jobs[i][0] <= time) {
                 queue.offer(new Job(jobs[i][0], jobs[i][1]));
                 i++;
+            }
+
+            if (!queue.isEmpty()) {
+                // queue에서 Job을 수행하고 time을 올리고 totalTime에 반영한다.
+                Job job = queue.poll();
+                time += job.workingTime;
+                totalTime += (time - job.requestTime);
+                count++;
+            } else {
+                time = jobs[i][0];
             }
         }
         return (int)(totalTime / jobs.length);
