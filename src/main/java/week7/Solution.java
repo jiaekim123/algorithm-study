f7889d7e04efa447311bb21a7487ed85ec641fba
SONG22 <ansongi527@gmail.com>
2021-01-01 22:25:00 +0900

    [week7] 등굣길 풀이

--- src/main/java/week7/Solution.java
+++ src/main/java/week7/Solution.java
@@ -5,8 +5,28 @@
 package week7;
 
 class Solution {
+
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        return answer;
+        int [][]d = new int [n][m];
+        for(int []p : puddles){
+            d[p[1]-1][p[0]-1]=-1;
+        }
+        d[0][0]=1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(d[i][j] == -1) {
+                    d[i][j] = 0;
+                    continue;
+                }
+                if(i!=0) d[i][j]+=d[i-1][j]%1000000007;
+                if(j!=0) d[i][j]+=d[i][j-1]%1000000007;
+//                else if(i==0 || j==0) d[i][j] = 1;
+//                else {
+//                    d[i][j] = d[i-1][j] + d[i][j-1];
+//                }
+            }
+        }
+
+        return  d[n-1][m-1]%1000000007;
     }
 }
