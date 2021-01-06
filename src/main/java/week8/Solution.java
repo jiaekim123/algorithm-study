2536e66607b481d97ccc332fcd917af169ee741a
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-06 22:19:33 +0900

    [week8][프로그래머스][월간 코드 챌린지 시즌1] 스타수열 문제 및 테스트 코드 추가
    - https://programmers.co.kr/learn/courses/30/lessons/70130

---
+++ src/main/java/week8/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/70130
+    [프로그래머스][월간 코드 챌린지 시즌1] 스타수열
+ */
+package week8;
+
+class Solution {
+    public int solution(int[] a) {
+        int answer = -1;
+        return answer;
+    }
+}
