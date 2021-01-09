bd54d6dd6ed49636a9ad18571fbe0f230a408219
SONG22 <ansongi527@gmail.com>
2021-01-10 01:41:26 +0900

    [week8] 스타수열 풀이

--- src/main/java/week8/Solution.java
+++ src/main/java/week8/Solution.java
@@ -4,44 +4,28 @@
  */
 package week8;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-
 class Solution {
     public int solution(int[] a) {
-        int answer =0;
-        LinkedList<Integer> save = new LinkedList<>();
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> retain = new HashSet<>();
-//        LinkedList<Integer> ans = new LinkedList<>();
-        for(int i=0; i<a.length; i++){
-            if(save.size()==0) save.add(a[i]);
-            else if(save.size()==1 && save.get(0)!=a[i]) {
-                int prev = save.remove();
-                if(retain.size()==0){
-                    retain.add(prev);
-                    retain.add(a[i]);
-//                    ans.add(prev);
-//                    ans.add(a[i]);
-                    answer+=2;
-                    continue;
-                }
-                set.add(prev);
-                set.add(a[i]);
-//                System.out.println(retain+" "+set);
-                retain.retainAll(set);
-                if(retain.size()==0){
-//                    System.out.println("교집합 없음 ");
-                    save.add(prev);
-                    continue;
-                }
-//                System.out.println("교집합 > "+retain+" "+set);
-                set.removeAll(set);
-                answer +=2;
-//                System.out.println("지우ㄱㅣ > "+retain+" "+set+" ans "+ans);
-            }
+        int answer = -1;
 
+        int []cnt = new int [a.length+1]; // a길이 미만의 수
+        for(int i=0; i<a.length; i++){
+            cnt[a[i]]++;
+        } // 숫자 등장 횟수
+
+        for(int i=0; i<cnt.length; i++){ // i를 공통 원소로 봤을 떄
+            if(cnt[i] == 0) continue; // 교집합이 될 수 없는 것
+            if(cnt[i]<= answer) continue; // 탐색 해보지 않아도 되는 경우
+            int res = 0;
+            for(int j=0; j<a.length-1; j++){ // 모든 원소에 대한 탐색
+                if(a[j]!=i && a[j+1] !=i) continue; // 공통원소가 없으면 pass
+                if(a[j]==a[j+1])continue; // 2개의 값이 달라야 함
+                res++;
+                j++;
+            }
+            answer = Math.max(answer, res); // 최대로 만들 수 있는 스타 수열 갯수 갱신
         }
-        return answer;
+
+        return answer*2; // 수열의 길이므로 *2
     }
 }
