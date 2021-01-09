d1db271f665c1590ad418e4111ea5b07077381f5
SONG22 <ansongi527@gmail.com>
2021-01-10 01:23:45 +0900

    [week8] 스타수열 오답...

--- src/main/java/week8/Solution.java
+++ src/main/java/week8/Solution.java
@@ -4,9 +4,44 @@
  */
 package week8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 class Solution {
     public int solution(int[] a) {
-        int answer = -1;
+        int answer =0;
+        LinkedList<Integer> save = new LinkedList<>();
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> retain = new HashSet<>();
+//        LinkedList<Integer> ans = new LinkedList<>();
+        for(int i=0; i<a.length; i++){
+            if(save.size()==0) save.add(a[i]);
+            else if(save.size()==1 && save.get(0)!=a[i]) {
+                int prev = save.remove();
+                if(retain.size()==0){
+                    retain.add(prev);
+                    retain.add(a[i]);
+//                    ans.add(prev);
+//                    ans.add(a[i]);
+                    answer+=2;
+                    continue;
+                }
+                set.add(prev);
+                set.add(a[i]);
+//                System.out.println(retain+" "+set);
+                retain.retainAll(set);
+                if(retain.size()==0){
+//                    System.out.println("교집합 없음 ");
+                    save.add(prev);
+                    continue;
+                }
+//                System.out.println("교집합 > "+retain+" "+set);
+                set.removeAll(set);
+                answer +=2;
+//                System.out.println("지우ㄱㅣ > "+retain+" "+set+" ans "+ans);
+            }
+
+        }
         return answer;
     }
 }
