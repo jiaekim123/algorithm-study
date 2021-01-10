8fdc51a4f436064f28fe878a2da7256b705c821e
SONG22 <ansongi527@gmail.com>
2021-01-10 23:29:24 +0900

    [week9] 가장 먼 노드 (줄 정렬 수정)

--- src/main/java/week9/Solution.java
+++ src/main/java/week9/Solution.java
@@ -11,30 +11,30 @@ import java.util.Queue;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        LinkedList<Integer> [] list = new LinkedList[n+1];
-        for(int i=0; i<=n; i++) list[i] = new LinkedList<>();
-        for(int[] e: edge){
+        LinkedList<Integer>[] list = new LinkedList[n + 1];
+        for (int i = 0; i <= n; i++) list[i] = new LinkedList<>();
+        for (int[] e : edge) {
             list[e[0]].add(e[1]);
             list[e[1]].add(e[0]);
         }
 
-        int []dis = new int [n+1];
+        int[] dis = new int[n + 1];
         Arrays.fill(dis, -1);
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
-        dis[1]=0;
+        dis[1] = 0;
         int max = 0;
-        while (!q.isEmpty()){
+        while (!q.isEmpty()) {
             int now = q.poll();
-            for(int next : list[now]){
-                if(dis[next]!=-1)continue;
-                dis[next]=dis[now]+1;
+            for (int next : list[now]) {
+                if (dis[next] != -1) continue;
+                dis[next] = dis[now] + 1;
                 max = Math.max(dis[next], max);
                 q.add(next);
             }
         }
-        for(int i=1;i<=n; i++){
-            if(max == dis[i]) answer++;
+        for (int i = 1; i <= n; i++) {
+            if (max == dis[i]) answer++;
         }
         return answer;
     }
