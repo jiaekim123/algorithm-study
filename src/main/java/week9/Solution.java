8566f15ff456fd75ee5a14f1f85a0c3088e3d184
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-10 19:43:18 +0900

    [week9][프로그래머스][그래프] 가장 먼 노드 문제 추가
    - https://programmers.co.kr/learn/courses/30/lessons/49189

---
+++ src/main/java/week9/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/49189
+    [프로그래머스][그래프] 가장 먼 노드
+ */
+package week9;
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+}
