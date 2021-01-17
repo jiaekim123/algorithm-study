7ce1fa0ca9f5218785fef02a97493552d771b018
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-17 22:19:33 +0900

    [week10][프로그래머스][2020 KAKAO BLIND RECRUITMENT] 자물쇠와 열쇠 문제 추가
    - https://programmers.co.kr/learn/courses/30/lessons/60059

---
+++ src/main/java/week10/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/60059
+    [프로그래머스][2020 KAKAO BLIND RECRUITMENT] 자물쇠와 열쇠
+ */
+package week10;
+
+class Solution {
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        return answer;
+    }
+}
