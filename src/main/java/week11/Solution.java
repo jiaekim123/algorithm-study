86f8a674f3fd282dec6b0d41bb419483e51eb076
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-30 13:46:23 +0900

    [week11][프로그래머스][2021 KAKAO BLIND RECRUITMENT] 순위 검색 문제 추가
    - https://programmers.co.kr/learn/courses/30/lessons/72412

---
+++ src/main/java/week11/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/72412
+    [프로그래머스][2021 KAKAO BLIND RECRUITMENT] 순위 검색
+ */
+package week11;
+
+class Solution {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
