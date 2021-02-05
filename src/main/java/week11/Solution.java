11a94a739e2c8b666fc411b451c02e838e55a636
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-05 21:12:17 +0900

    [week11][프로그래머스][2021 KAKAO BLIND RECRUITMENT] 순위 검색 문제 - Developer Class -> String 배열로 변경 (효율성 timeout)

--- src/main/java/week11/Solution.java
+++ src/main/java/week11/Solution.java
@@ -10,10 +10,10 @@ import java.util.List;
 class Solution {
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        List<Developer> list = new ArrayList<>();
+        List<String[]> list = new ArrayList<>();
         for (int i = 0; i < info.length; i++) {
             String[] results = info[i].split(" ");
-            list.add(new Developer(results[0], results[1], results[2], results[3], Integer.parseInt(results[4])));
+            list.add(new String[]{results[0], results[1], results[2], results[3], results[4]});
         }
 
         for (int i = 0; i < query.length; i++) {
@@ -31,45 +31,12 @@ class Solution {
         }
         return answer;
     }
-    private boolean isPass(Developer developer, String[] conditions){
-        if (!conditions[0].equals("-") && !developer.language.equals(Language.valueOf(conditions[0]))) return false;
-        if (!conditions[1].equals("-") && !developer.type.equals(Type.valueOf(conditions[1]))) return false;
-        if (!conditions[2].equals("-") && !developer.career.equals(Career.valueOf(conditions[2]))) return false;
-        if (!conditions[3].equals("-") && !developer.soulFood.equals(SoulFood.valueOf(conditions[3]))) return false;
-        if (!conditions[4].equals("-") && developer.score < Integer.parseInt(conditions[4])) return false;
+    private boolean isPass(String[] developer, String[] conditions){
+        if (!conditions[0].equals("-") && !developer[0].equals(conditions[0])) return false;
+        if (!conditions[1].equals("-") && !developer[1].equals(conditions[1])) return false;
+        if (!conditions[2].equals("-") && !developer[2].equals(conditions[2])) return false;
+        if (!conditions[3].equals("-") && !developer[3].equals(conditions[3])) return false;
+        if (!conditions[4].equals("-") && Integer.parseInt(developer[4]) < Integer.parseInt(conditions[4])) return false;
         return true;
     }
-
-    static class Developer {
-        Language language;
-        Type type;
-        Career career;
-        SoulFood soulFood;
-        int score;
-
-        Developer(String language, String type, String career, String soulFood, int score) {
-            this.language = Language.valueOf(language);
-            this.type = Type.valueOf(type);
-            this.career = Career.valueOf(career);
-            this.soulFood = SoulFood.valueOf(soulFood);
-            this.score = score;
-        }
-    }
-
-    enum Language {
-        cpp, java, python;
-    }
-
-    enum Type {
-        backend, frontend;
-    }
-
-    enum Career {
-        junior, senior;
-    }
-
-    enum SoulFood {
-        chicken, pizza;
-    }
-
 }
