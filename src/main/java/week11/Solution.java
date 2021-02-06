1b95924716fb9765708703b63c54aed16e4be829
SONG22 <ansongi527@gmail.com>
2021-02-07 01:59:53 +0900

    [week11] 2차시도 시간초과

--- src/main/java/week11/Solution.java
+++ src/main/java/week11/Solution.java
@@ -4,39 +4,66 @@
  */
 package week11;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] info, String[] query) {
+        // 1. info를 순회하면서 문자열 파싱 후 모든 경우의 수에 점수를 넣는다
+        Map<String, List<Integer>> infos = new HashMap<>();
+        for (int i = 0; i < info.length; i++) {
+            String[] s = info[i].split(" ");
+            Integer value = Integer.parseInt(s[4]);
+            make(s, 0, "", infos);
+        }
+
+        // 2.모든 map의 key에 대해서 순회하면서 오름차순 정렬
+        for(Map.Entry<String, List<Integer>> e : infos.entrySet()){
+            Collections.sort(e.getValue());
+        }
 
         int[] answer = new int[query.length];
-        for (int t = 0; t < query.length; t++) {
-            String[] q = query[t].split("and");
 
-            String lang = q[0].contains("-") ? "" : q[0].replace(" ", "");
-            String position = q[1].contains("-") ? "" : q[1].replace(" ", "");
-            String career = q[2].contains("-") ? "" : q[2].replace(" ", "");
+        // 3. query 를 순회하면서 query정보 문자열 파싱
+        for (int i = 0; i < query.length; i++) {
+            String[] s = query[i].replace("and ", "").split(" ");
+            int score = Integer.parseInt(s[4]);
+            String key = s[0] + s[1] + s[2] + s[3];
+            if(! infos.containsKey(key)) continue;
+            List<Integer> scores = infos.get(key);
 
-            String[] q2 = q[3].split(" ");
-
-            String food = q2[1].contains("-") ? "" : q2[1].replace(" ", "");
-            int score = Integer.parseInt(q2[2]);
-            System.out.println("Query " + lang + " " + position + " " + career + " " + food + " " + score);
-
-
-            Stream<String> ans = Arrays.stream(info).filter((s) -> {
-                int jumsu = Integer.parseInt(s.split(" ")[4]);
-                if (s.contains(lang) && s.contains(position) && s.contains(career) && s.contains(food) && jumsu >= score) {
-                    System.out.println(s);
-                    return true;
-                }
-                return false;
-            });
-            answer[t] = ans.toArray().length;
-            System.out.println(answer[t]);
+            answer[i] = scores.size() - upper_bound(scores, score);
         }
         return answer;
+    }
+
+    public int upper_bound(List<Integer> scores, int target) {
+        int left = 0;
+        int right = scores.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (scores.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public void make(String[] arr, int start, String res, Map<String, List<Integer>> map) {
+        if (start == 4) {
+//            System.out.println(res);
+            int score = Integer.parseInt(arr[4]);
+            if(map.containsKey(res)) map.get(res).add(score);
+            else {
+                LinkedList<Integer> list = new LinkedList<>();
+                list.add(score);
+                map.put(res, list);
+            }
+            return;
+        }
+        make(arr, start + 1, res + "-", map);
+        make(arr, start + 1, res + arr[start], map);
+
     }
 }
