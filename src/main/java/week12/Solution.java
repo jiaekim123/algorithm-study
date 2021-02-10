09a5c3cda40871a64189df42428efc52c3f313cd
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-10 11:41:53 +0900

    [week12][프로그래머스][탐욕법(Greedy)] 단속 카메라 문제 추가

---
+++ src/main/java/week12/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/42884#
+    [프로그래머스][탐욕법(Greedy)] 단속 카메라
+ */
+package week12;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        return answer;
+    }
+}
