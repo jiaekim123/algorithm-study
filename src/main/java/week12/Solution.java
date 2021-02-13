ff2b1c9f1443b3fafb8a446716a330c4efb8546f
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-13 22:28:44 +0900

    [week12][프로그래머스][탐욕법(Greedy)] 단속 카메라 문제 해결

--- src/main/java/week12/Solution.java
+++ src/main/java/week12/Solution.java
@@ -4,9 +4,40 @@
  */
 package week12;
 
+import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
+        int answer = 1;
+        List<Car> list = new ArrayList<>();
+        for (int[] route : routes) {
+            list.add(new Car(route[0], route[1]));
+        }
+        Collections.sort(list);
+        Car before = list.get(0);
+        for (int i = 1; i<list.size(); i++){
+            Car next = list.get(i);
+            if (before.endTime >= next.endTime) {
+                before = next;
+            } else if (before.endTime < next.startTime){
+                answer++;
+                before = next;
+            }
+        }
+
         return answer;
+    }
+    static class Car implements Comparable<Car>{
+        int startTime;
+        int endTime;
+        Car (int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        public int compareTo(Car o) {
+            return this.startTime - o.startTime;
+        }
     }
 }
