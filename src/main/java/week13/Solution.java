59f7af03fe94d57558687411964a1920a87acd26
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-14 12:02:06 +0900

    [week13][프로그래머스][2020 KAKAO BLIND RECRUITMENT] 괄호 변환

---
+++ src/main/java/week13/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/60058
+    [프로그래머스][2020 KAKAO BLIND RECRUITMENT] 괄호 변환
+ */
+package week13;
+
+class Solution {
+    public String solution(String p) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ src/test/java/week13/SolutionTest.java
@@ -0,0 +1,15 @@
+package week13;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution() {
+        assertEquals("(()())()", new Solution().solution("(()())()"));
+        assertEquals("()", new Solution().solution(")("));
+        assertEquals("()(())()", new Solution().solution("()))((()"));
+    }
+}
