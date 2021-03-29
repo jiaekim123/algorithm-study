b37fee21f470669081ada9391754ef37c79f6916
SONG22 <ansongi527@gmail.com>
2021-03-29 22:06:54 +0900

    [week17] 17번째 문제

---
+++ src/main/java/week17/Solution.java
@@ -0,0 +1,11 @@
+package week17;
+/*
+https://programmers.co.kr/learn/courses/30/lessons/72411
+ * */
+
+public class Solution {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
