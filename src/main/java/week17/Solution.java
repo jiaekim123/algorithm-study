56f78aff459607dbacd4a7dbc7b520a361148727
SONG22 <ansongi527@gmail.com>
2021-03-31 00:20:37 +0900

    [week17] 1차시도 실패 - 시간초과 85/100

--- src/main/java/week17/Solution.java
+++ src/main/java/week17/Solution.java
@@ -3,9 +3,72 @@ package week17;
 https://programmers.co.kr/learn/courses/30/lessons/72411
  * */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Solution {
+    String[] array;
+
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
+        HashSet<String> set = new HashSet<>();
+        for (String o : orders) {
+            String[] arr = o.split("");
+            for (String a : arr) {
+                set.add(a);
+            }
+        }
+        array = set.stream().toArray(String[]::new); // stream 문법
+        String ans = "";
+        for (int c : course) {
+            ArrayList<String> list = new ArrayList<>();
+            combi(c, "", 0, list);
+            if (!ans.equals("")) ans += " ";
+            ans += solve(orders, list);
+
+        }
+        String[] answer = ans.split(" ");
+        Arrays.sort(answer);
         return answer;
+    }
+
+    // 글자수 n 개인 조합
+    public void combi(int n, String ret, int start, List<String> list) {
+        if (ret.length() == n) {
+            list.add(ret);
+            return;
+        }
+
+        for (int i = start; i < array.length; i++) {
+            combi(n, ret + array[i], i + 1, list);
+        }
+    }
+
+    // orders 를 순회하면서 어떤 문자 조합이 몇번 반복되는지 확인 후 최대
+    public String solve(String[] orders, List<String> menus) {
+        int[] menu_count = new int[menus.size()];
+        int max = 1;
+        String menu = "";
+        for (int i = 0; i < menus.size(); i++) {
+            for (String order : orders) {
+                if (check(order, menus.get(i))) menu_count[i]++;
+            }
+            if (max < menu_count[i]) {
+                max = menu_count[i];
+                menu = menus.get(i);
+            } else if (max != 1 && max == menu_count[i]) {
+                menu += " " + menus.get(i);
+            }
+        }
+        return menu;
+    }
+
+    public boolean check(String order, String menu) {
+        String[] menu_arr = menu.split("");
+        if (menu.length() > order.length()) return false;
+        for (String m : menu_arr)
+            if (!order.contains(m)) return false;
+        return true;
     }
 }
--- src/test/java/week17/SolutionTest.java
+++ src/test/java/week17/SolutionTest.java
@@ -2,17 +2,17 @@ package week17;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SolutionTest {
 
     @Test
     void solution() {
-        assertEquals(new String[]{"AC", "ACDE", "BCFG", "CDE"},
+        assertArrayEquals(new String[]{"AC", "ACDE", "BCFG", "CDE"},
                 new Solution().solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}));
-        assertEquals(new String[]{"ACD", "AD", "ADE", "CD", "XYZ"},
+        assertArrayEquals(new String[]{"ACD", "AD", "ADE", "CD", "XYZ"},
                 new Solution().solution(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}));
-        assertEquals(new String[]{"WX", "XY"},
+        assertArrayEquals(new String[]{"WX", "XY"},
                 new Solution().solution(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}));
     }
 }
